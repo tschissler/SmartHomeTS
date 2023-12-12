9bd9612121fcefbcd914ff81c6955b1c3e447414
Thomas Schissler <tschissler@agilemax.de>
2023-12-12 02:16:22 +0100

    Added OTA update

--- ESP32Firmwares/LEDStripe.Firmware/src/main.cpp
+++ ESP32Firmwares/LEDStripe.Firmware/src/main.cpp
@@ -2,6 +2,11 @@
 #include <FastLED.h>
 #include <WiFi.h>
 #include <WebServer.h>
+#include <PubSubClient.h>
+// #include <ArduinoOTA.h>
+//#include <HTTPClient.h>
+//#include <Update.h>
+#include <ESP32httpUpdate.h>
 
 #define DATA_PIN_1 13
 #define DATA_PIN_2 12
@@ -13,7 +18,14 @@ CRGB leds[NUM_LEDS];
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PASSWORD;
 
+// MQTT Broker settings
+const char* mqtt_broker = "smarthomepi2";
+const int mqtt_port = 32004;
+const char* mqtt_topic = "OTAUpdateLEDStripeTopic";
+
 WebServer server(80);
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
 
 void setColor() {
   // Check if each parameter exists
@@ -64,6 +76,64 @@ void initLED() {
   FastLED.show();
 }
 
+void updateFirmwareFromUrl(const String &firmwareUrl) {
+    Serial.print("Downloading new firmware from: ");
+    Serial.println(firmwareUrl);
+    
+    t_httpUpdate_return ret = ESPhttpUpdate.update(firmwareUrl);
+    
+    switch(ret) {
+        case HTTP_UPDATE_FAILED:
+            Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+            break;
+
+        case HTTP_UPDATE_NO_UPDATES:
+            Serial.println("HTTP_UPDATE_NO_UPDATES");
+            break;
+
+        case HTTP_UPDATE_OK:
+            Serial.println("HTTP_UPDATE_OK");
+                Serial.println("Update done");
+            break;
+    }
+    Serial.println();
+}
+
+void connectToMQTT() {
+    mqttClient.setServer(mqtt_broker, mqtt_port);
+    while (!mqttClient.connected()) {
+        if (mqttClient.connect("ESP32LEDStripeClient")) {
+            mqttClient.subscribe(mqtt_topic);
+            Serial.println("Connected to MQTT Broker");
+        } else {
+            Serial.print("Failed to connect to MQTT Broker: ");
+            Serial.println(mqtt_broker);
+            delay(5000);
+        }
+    }
+}
+
+void mqttCallback(char* topic, byte* message, unsigned int length) {
+    Serial.print("Message arrived on topic: ");
+    Serial.print(topic);
+    Serial.print(". Message: ");
+
+    String messageTemp;
+
+    for (int i = 0; i < length; i++) {
+        messageTemp += (char)message[i];
+    }
+
+    Serial.println(messageTemp);
+
+    if (String(topic) == mqtt_topic) {
+          // Trigger OTA Update
+          Serial.println("OTA Update Triggered");
+          String firmwareUrl = messageTemp;
+          updateFirmwareFromUrl(firmwareUrl);
+    }
+}
+
 void setup() {
   FastLED.addLeds<WS2812B, DATA_PIN_1, GRB>(leds, NUM_LEDS);
   FastLED.addLeds<WS2812B, DATA_PIN_2, GRB>(leds, NUM_LEDS);
@@ -85,6 +155,10 @@ void setup() {
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 
+  // Set up MQTT
+  mqttClient.setCallback(mqttCallback);
+  connectToMQTT();
+
   // Define endpoint
   server.on("/setColor", HTTP_GET, setColor);
 
@@ -93,5 +167,10 @@ void setup() {
 }
 
 void loop() {
+  if (!mqttClient.connected()) {
+    connectToMQTT();
+  }
+  mqttClient.loop();
+
   server.handleClient();
 }
