05c1d13a67feb5d1ff90ab10f140f4bce6a86a38
Thomas Schissler <tschissler@agilemax.de>
2023-12-27 10:29:26 +0100

    Added option to read data and clear table

--- StorageController/CosmosDBController.cs
+++ StorageController/CosmosDBController.cs
@@ -1,6 +1,7 @@
 ﻿
 using Azure.Data.Tables;
 using Azure.Data.Tables.Models;
+using DataContracts;
 using System.Runtime.InteropServices;
 
 namespace StorageController
@@ -8,18 +9,21 @@ namespace StorageController
     public class CosmosDBController
     {
         private TableClient tableClient;
+        private TableServiceClient serviceClient;
+        private string tableName;
 
         public CosmosDBController(string storageUri, string tableName, string accountName, string storageAccountKey)
         {
-            var serviceClient = new TableServiceClient(
+            serviceClient = new TableServiceClient(
                 new Uri(storageUri),
                 new TableSharedKeyCredential(accountName, storageAccountKey));
             serviceClient.CreateTableIfNotExists(tableName);
 
             tableClient = serviceClient.GetTableClient(tableName);
+            this.tableName = tableName;
         }
 
-        public void WriteData(string partitionKey, string rowKey, decimal value, DateTime time)
+        public void WriteData(string partitionKey, string rowKey, double value, DateTime time)
         {
             // Create new Entity and write to table
             var entity = new TableEntity(partitionKey.Replace("/", "_"), rowKey)
@@ -28,6 +32,26 @@ namespace StorageController
                 { "Time", time.ToUniversalTime() }
             };
             tableClient.AddEntity(entity);
+        }
+
+        public DataValueTableEntity ReadTop1Data(string filer)
+        {
+            // Read data from table
+            return tableClient.Query<DataValueTableEntity>(filer, 1).FirstOrDefault();
+        }
+
+        public List<DataValueTableEntity> ReadData(string filer)
+        {
+            // Read data from table
+            return tableClient.Query<DataValueTableEntity>(filer).ToList();
+        }
+
+        public void ClearTable()
+        {
+            tableClient.DeleteAsync();
+            Thread.Sleep(1000);
+            serviceClient.CreateTable(tableName);
+            tableClient = serviceClient.GetTableClient(tableName); 
         }
     }
 }
