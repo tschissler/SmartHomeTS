a5ddf38c3b8bfb58c10026e89a1f9f1a72433b6c
Thomas Schissler <tschissler@agilemax.de>
2023-12-27 14:56:33 +0100

    Added test to recalculate rowkey to make order descending

---
+++ StorageConnector/StorageConnectorTests/RecalculateRowKeysTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StorageConnector;
+using StorageController;
+using System;
+
+namespace StorageConnectorTests
+{
+    [TestClass]
+    public class RecalculateRowKeysTests
+    {
+        private string storageUri = "https://smarthometsstorage.table.cosmos.azure.com:443/";
+        private string storageAccountName = "smarthometsstorage";
+        private string storageAccountKey = "yRZ84NCODris5jSJpP1tbZO1zxVkTTRSEsn4Yiu5TNyKFIToLOaMDe6whunduEzFT3tFwm95X4lcACDbRQDdPQ==";
+        private string tableName = "SmartHomeData";
+
+        [Ignore]
+        [TestMethod]
+        public void RecalculateRowKeys()
+        {
+            var controller = new CosmosDBController(storageUri, tableName, storageAccountName, storageAccountKey);
+            var data = controller.ReadData("RowKey lt '50000000000000'");
+
+            foreach (var item in data)
+            {
+                try
+                {
+                    controller.WriteData(item.PartitionKey, Converters.ConvertDateTimeToReverseRowKey(item.Time), item.Value, item.Time);
+                    controller.DeleteData(item.PartitionKey, item.RowKey);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(item.PartitionKey + " - " + item.RowKey);
+                    throw ex;
+                }
+            }
+        }
+    }
+}
