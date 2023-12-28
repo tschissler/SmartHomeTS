49cc02509be323362641350082ff83d760d567c3
Thomas Schissler <tschissler@agilemax.de>
2023-12-28 15:02:43 +0100

    Fixed test by defining UTC time

--- StorageConnector/StorageConnectorTests/ConvertersTestsConvertDateTimeToReverseRowKeyTests.cs
+++ StorageConnector/StorageConnectorTests/ConvertersTestsConvertDateTimeToReverseRowKeyTests.cs
@@ -10,7 +10,7 @@ namespace StorageConnectorTests
         public void ConvertValidDate()
         {
             // Arrange
-            var time = new DateTime(2023, 12, 18, 19, 9, 21);
+            var time = new DateTime(2023, 12, 18, 18, 9, 21, DateTimeKind.Utc);
 
             // Act
             var result = Converters.ConvertDateTimeToReverseRowKey(time);
