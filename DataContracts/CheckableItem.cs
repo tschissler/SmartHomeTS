188fb7b7a0627d7a0c89dec6fc0a1bdb1e297b72
Thomas Schissler <tschissler@agilemax.de>
2023-12-28 20:17:17 +0100

    Optimized layout

--- DataContracts/CheckableItem.cs
+++ DataContracts/CheckableItem.cs
@@ -4,6 +4,7 @@ namespace DataContracts
 {
     public class CheckableItem
     {
+        public string Id { get; set; }
         public string PartitionKey { get; set; }
         public string Label { get; set; }
         public bool IsChecked { get; set; }
