7dd474aa0d63d441dbd4cadf7863758cb2eaf62b
Thomas Schissler <tschissler@agilemax.de>
2023-12-28 17:32:51 +0100

    Refacored loading data

--- DataContracts/CheckableItem.cs
+++ DataContracts/CheckableItem.cs
@@ -1,9 +1,12 @@
-﻿namespace DataContracts
+﻿using System.Drawing;
+
+namespace DataContracts
 {
     public class CheckableItem
     {
         public string PartitionKey { get; set; }
         public string Label { get; set; }
         public bool IsChecked { get; set; }
+        public string Color { get; set; }
     }
 }
