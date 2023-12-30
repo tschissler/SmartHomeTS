adc659bae12a021b3b74a304cbe862abf999b37a
Thomas Schissler <tschissler@agilemax.de>
2023-12-30 15:03:09 +0100

    Added display support

--- ESP32Firmwares/TemperatureSensor.Firmware/src/main.cpp
+++ ESP32Firmwares/TemperatureSensor.Firmware/src/main.cpp
@@ -3,14 +3,35 @@
 #include <MQTT.h>
 #include "DHT.h"
 #include "AzureOTAUpdater.h"
+#include <WiFiUdp.h>
+#include <NTPClient.h>
+#include <ESP32Ping.h>
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+
+// Pin configuration
+#define TFT_CS     15
+#define TFT_RST    4  
+#define TFT_DC     2
+#define TFT_MOSI   23
+#define TFT_SCLK   18
+
+// Initialize Adafruit ST7735
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+
 
 const char* version = TEMPSENSORFW_VERSION;
 String chipID = "";
 
 // Deep Sleep Configuration
-#define TIME_TO_SLEEP  60        // Time in seconds for ESP32 to sleep
+#define BLINK_DURATION 100       // Blink duration in milliseconds, blinking will happen every second
+#define MQTT_MESSAGE_FREQUENCY  60        // Number of seconds before sending MQTT message
+static int MQTT_MESSAGE_FREQUENCY_COUNTER = 1000;
 
-#define INTERAL_LED_PIN 2        // Internal LED pin
+#define LED_RED_PIN 32
+#define LED_GREEN_PIN 26
+#define LED_BLUE_PIN 33
 #define DHTPIN 25     
 #define DHTTYPE DHT22   
 DHT dht(DHTPIN, DHTTYPE);
@@ -27,9 +48,14 @@ const int mqtt_port = 32004;
 const char* mqtt_OTAtopic = "OTAUpdateTemperatureSensor";
 
 WiFiClient espClient;
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
 MQTTClient mqttClient;
 
 static bool otaInProgress = false;
+static bool otaEnable = false;
+static bool sendMQTTMessages = true;
+static bool mqttSuccess = false;
 
 String extractVersionFromUrl(String url) {
     int lastUnderscoreIndex = url.lastIndexOf('_');
@@ -42,8 +68,44 @@ String extractVersionFromUrl(String url) {
     return ""; // Return empty string if the pattern is not found
 }
 
+void printInformationOnTFT(String temperature, String humidity, bool mqttMessage) {
+  // Clear TFT screen
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setCursor(0, 0);
+
+  tft.println("TemperatureSensor");
+  tft.print("Version: ");
+  tft.println(version);
+  tft.print("Chip ID: ");
+  tft.println(chipID);
+
+  tft.setCursor(0, 30);
+
+  // Update NTP client
+  timeClient.update();
+
+  // Get current time
+  String formattedTime = timeClient.getFormattedTime();
+  tft.println(formattedTime);
+  tft.println("Temperature: " + temperature + "C");
+  tft.println("Humidity: " + humidity + "%");
+  tft.println("IP: " + WiFi.localIP().toString());
+  tft.println("SSID: " + ssid);
+  tft.println("RSSI: " + String(WiFi.RSSI()) + " dBm");
+
+  if (mqttMessage) {
+    tft.println();
+    if (sendMQTTMessages) {
+      tft.println(mqttSuccess?"MQTT message sent\nsuccessfully":"Sending MQTT\nmessage failed");
+    } else {
+      tft.println("MQTT messages disabled");
+    }
+  }
+
+  tft.println(Ping.ping("smarthomepi2")?"Ping successful":"Ping failed");
+}
 void mqttCallback(String topic, String &payload) {
-    if (otaInProgress)
+    if (otaInProgress || !otaEnable)
       return;
     
     Serial.print("Message arrived on topic: ");
@@ -73,11 +135,11 @@ void mqttCallback(String topic, String &payload) {
 
 void connectToMQTT() {
     mqttClient.begin(mqtt_broker, mqtt_port, espClient);
+    mqttClient.setKeepAlive(5);
     
     while (!mqttClient.connected()) {
         if (mqttClient.connect("ESP32TemperatureSensorClient")) {
             mqttClient.subscribe(mqtt_OTAtopic);
-            Serial.println("Connected to MQTT Broker from connectToMQTT");
         } else {
             Serial.print("Failed to connect to MQTT Broker: ");
             Serial.println(mqtt_broker);
@@ -103,7 +165,6 @@ void reconnect() {
     while (!mqttClient.connected()) {
         if (mqttClient.connect("ESP32TemperatureSensorClient")) {
             mqttClient.subscribe(mqtt_OTAtopic);
-            Serial.println("Connected to MQTT Broker from reconnect");
         } else {
             Serial.print("Failed to connect to MQTT Broker: ");
             Serial.println(mqtt_broker);
@@ -133,13 +194,23 @@ void readSensorAndPublish() {
   dtostrf(temperature, 1, 2, tempString);
   dtostrf(humidity, 1, 2, humString);
 
-  String baseTopic = "data/" + chipID + "/"; // Base topic with the chipID
-  
-  mqttClient.publish((baseTopic + "temperature").c_str(), String(tempString), true, 2);
-  mqttClient.publish((baseTopic + "humidity").c_str(), String(humString), true, 2);
-  mqttClient.publish(("meta/" + chipID + "/version").c_str(), String(version), true, 2);
-  Serial.println("Published new values to MQTT Broker");
+  if (sendMQTTMessages)
+  {
+    String baseTopic = "data/" + chipID + "/"; // Base topic with the chipID
+
+    if (!mqttClient.connected()) {
+      Serial.println("MQTT Client not connected, reconnecting before publish...");
+      reconnect();
+    }
+    
+    mqttSuccess =  mqttClient.publish((baseTopic + "temperature").c_str(), String(tempString), true, 2);
+    mqttClient.publish((baseTopic + "humidity").c_str(), String(humString), true, 2);
+    mqttClient.publish(("meta/" + chipID + "/version").c_str(), String(version), true, 2);
+    Serial.println(mqttSuccess?"Published new values to MQTT Broker":"Publishing to MQTT Broker failed");
+    Serial.println(" -> Connected:" + String(mqttClient.connected()) + " -> LastError:"  + String(mqttClient.lastError())  + " -> ReturnCode:" + String(mqttClient.returnCode()));
+  }
   Serial.println("Temperature: " + String(temperature) + "°C, Humidity: " + String(humidity) + "%, Version: " + version);
+  printInformationOnTFT(String(temperature), String(humidity), true);
 }
 
 void findWifi() {
@@ -211,6 +282,15 @@ void setup() {
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 
+  // Initialize display
+  tft.initR(INITR_BLACKTAB);      // Initialize a ST7735S chip, black tab
+  tft.fillScreen(ST77XX_BLACK);   // Fill screen with black color
+  tft.setRotation(1);             // Set orientation (0-3)
+
+  // Text settings
+  tft.setTextSize(1);             // Set text size
+  tft.setTextColor(ST77XX_WHITE); // Set text color
+
   // // Set up MQTT
   connectToMQTT();
 
@@ -218,18 +298,56 @@ void setup() {
   dht.begin();
   Serial.println("DHT sensor initialized");
 
-  pinMode(INTERAL_LED_PIN, OUTPUT);
-  digitalWrite(INTERAL_LED_PIN, LOW);
+  pinMode(LED_RED_PIN, OUTPUT);
+  pinMode(LED_GREEN_PIN, OUTPUT);
+  pinMode(LED_BLUE_PIN, OUTPUT);
+  
+  // Initialize NTPClient
+  timeClient.begin();
+  timeClient.setTimeOffset(0); // Set your time offset from UTC in seconds
+
+  // Update NTP client
+  timeClient.update();
+
+  printInformationOnTFT("-", "-", false);
 }
 
 void loop() {
   otaInProgress = AzureOTAUpdater::CheckUpdateStatus();
-  if (!mqttClient.connected()) {
-    Serial.println("MQTT Client not connected, reconnecting...");
-    reconnect();
-  }
-  readSensorAndPublish();
-  mqttClient.loop();
 
-  delay(TIME_TO_SLEEP * 1000);
+  if (MQTT_MESSAGE_FREQUENCY_COUNTER >= MQTT_MESSAGE_FREQUENCY) {
+    MQTT_MESSAGE_FREQUENCY_COUNTER = 0;
+    if (!mqttClient.connected()) {
+      Serial.println("MQTT Client not connected, reconnecting in loop...");
+      reconnect();
+    }
+
+    readSensorAndPublish();
+    mqttClient.loop();
+  } else {
+    MQTT_MESSAGE_FREQUENCY_COUNTER++;
+  }
+
+  // if(Ping.ping("smarthomepi2")) {
+  //   digitalWrite(LED_BLUE_PIN, HIGH);
+  //   delay(BLINK_DURATION);
+  //   digitalWrite(LED_BLUE_PIN, LOW);
+  //   delay(BLINK_DURATION);
+  // } else {
+  //   digitalWrite(LED_RED_PIN, HIGH);
+  //   delay(BLINK_DURATION);
+  //   digitalWrite(LED_RED_PIN, LOW);
+  //   delay(BLINK_DURATION);
+  // }
+
+  // if (mqttSuccess) {
+  //   digitalWrite(LED_GREEN_PIN, HIGH);
+  //   delay(BLINK_DURATION);
+  //   digitalWrite(LED_GREEN_PIN, LOW);
+  // } else {
+  //   digitalWrite(LED_RED_PIN, HIGH);
+  //   delay(BLINK_DURATION);
+  //   digitalWrite(LED_RED_PIN, LOW);
+  // }
+  delay(1000-3*BLINK_DURATION);
 }
