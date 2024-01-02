c85a549c6aa7f4ddc19d55d4a9305474f6df26c8
Thomas Schissler <tschissler@agilemax.de>
2024-01-02 14:57:56 +0100

    Empty Blazor MAUI app

---
+++ SmartHome.Web/SmartHomeBlazorApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartHomeBlazorApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
