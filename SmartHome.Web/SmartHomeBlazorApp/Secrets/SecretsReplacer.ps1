815b46d37c66a5634435228134e8817844a5d83a
Thomas Schissler <tschissler@agilemax.de>
2024-01-05 12:21:49 +0100

    Added access to table storage

---
+++ SmartHome.Web/SmartHomeBlazorApp/Secrets/SecretsReplacer.ps1
@@ -0,0 +1,7 @@
+﻿$templatePath = "SecretsTemplate.cs"
+$outputPath = "Secrets.cs"
+
+$content = Get-Content $templatePath
+$content = $content -replace "<The Uri to the Azure Table Storage>", $env:STORAGE_URI
+$content = $content -replace "<The key to access the Azure Table Storage>", $env:STORAGE_KEY
+$content | Set-Content $outputPath
---
+++ SmartHome.Web/SmartHomeBlazorApp/Secrets/SecretsTemplate.cs
@@ -0,0 +1,10 @@
+﻿// Use this file to create a Secrets.cs file in the same directory.
+// Then, add your secrets to the Secrets class.
+namespace SmartHomeBlazorApp.Secrets
+{
+    internal class StorageSecrets
+    {
+        internal const string StorageUri = "<The Uri to the Azure Table Storage>"; 
+        internal const string StorageKey = "<The key to access the Azure Table Storage>";
+    }
+}
