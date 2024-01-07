283d26d2b8bad76e5a5c7f73d969b7d219d02053
Thomas Schissler <tschissler@agilemax.de>
2024-01-07 21:37:04 +0100

    Added window status display

--- DataContracts/DataValueTableEntity.cs
+++ DataContracts/DataValueTableEntity.cs
@@ -13,4 +13,15 @@ namespace DataContracts
         public DateTimeOffset? Timestamp { get; set; }
         public ETag ETag { get; set; }
     }
+
+    public class StringDataValueTableEntity : ITableEntity
+    {
+        public string Value { get; set; }
+        public DateTime Time { get; set; }
+        public DateTime LocalTime { get { return Time.ToLocalTime(); } }
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+    }
 }
