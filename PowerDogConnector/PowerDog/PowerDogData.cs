94c0e9030986ff3ac5f4e7bad7e9735b1ea1edb1
Thomas Schissler <tschissler@agilemax.de>
2024-03-03 22:06:26 +0100

    Added PowerDog Connector

---
+++ PowerDogConnector/PowerDog/PowerDogData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerDogLib
+{
+    public class PowerDogData
+    {
+        public double PVProduction { get; set; }
+        public double GridSupply { get; set; }
+        public double GridDemand { get; set; }
+    }
+}
---
+++ SmartHomeHelpers/Logging/ConsoleHelpers.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+namespace SmartHomeHelpers.Logging
+{
+    public class ConsoleHelpers
+    {
+        static ConsoleHelpers()
+        {
+            Trace.Listeners.Add(new ConsoleTraceListener());
+        }
+
+        public static void PrintErrorMessage(string message)
+        {
+            Trace.TraceError($"{GetTimeStampText()} - {message}");
+        }
+
+        public static void PrintInformation(string message)
+        {
+            Trace.TraceInformation($"{GetTimeStampText()} - {message}");
+        }
+
+        public static void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        private static string GetTimeStampText()
+        {
+            return $"{DateTime.Now.ToShortDateString()}-{DateTime.Now.ToLongTimeString()}";
+        }
+    }
+}
