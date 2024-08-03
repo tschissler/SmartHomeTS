08488a63e08c1b45da1181026db713018b070349
Thomas Schissler <tschissler@agilemax.de>
2024-08-03 16:49:57 +0200

    Fixed update

--- ESP32Firmwares/Kellerdevice.Firmware/src/main.cpp
+++ ESP32Firmwares/Kellerdevice.Firmware/src/main.cpp
@@ -24,11 +24,53 @@ const int Red_LED_Pin = 13;
 const int Green_LED_Pin = 12;
 const int Blue_LED_Pin = 27;
 
+const char* ca_cert PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIFrDCCBJSgAwIBAgIQCfluwpVVXyR0nq8eXc7UnTANBgkqhkiG9w0BAQwFADBh
+MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3
+d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBH
+MjAeFw0yMzA2MDgwMDAwMDBaFw0yNjA4MjUyMzU5NTlaMF0xCzAJBgNVBAYTAlVT
+MR4wHAYDVQQKExVNaWNyb3NvZnQgQ29ycG9yYXRpb24xLjAsBgNVBAMTJU1pY3Jv
+c29mdCBBenVyZSBSU0EgVExTIElzc3VpbmcgQ0EgMDQwggIiMA0GCSqGSIb3DQEB
+AQUAA4ICDwAwggIKAoICAQDBeUy13eRZ/QC5bN7/IOGxodny7Xm2BFc88d3cca3y
+HyyVx1Y60+afY6DAo/2Ls1uzAfbDfMzAVWJazPH4tckaItDv//htEbbNJnAGvZPB
+4VqNviwDEmlAWT/MTAmzXfTgWXuUNgRlzZbjoFaPm+t6iJ6HdvDpWQAJbsBUZCga
+t257tM28JnAHUTWdiDBn+2z6EGh2DA6BCx04zHDKVSegLY8+5P80Lqze0d6i3T2J
+J7rfxCmxUXfCGOv9iQIUZfhv4vCb8hsm/JdNUMiomJhSPa0bi3rda/swuJHCH//d
+wz2AGzZRRGdj7Kna4t6ToxK17lAF3Q6Qp368C9cE6JLMj+3UbY3umWCPRA5/Dms4
+/wl3GvDEw7HpyKsvRNPpjDZyiFzZGC2HZmGMsrZMT3hxmyQwmz1O3eGYdO5EIq1S
+W/vT1yShZTSusqmICQo5gWWRZTwCENekSbVX9qRr77o0pjKtuBMZTGQTixwpT/rg
+Ul7Mr4M2nqK55Kovy/kUN1znfPdW/Fj9iCuvPKwKFdyt2RVgxJDvgIF/bNoRkRxh
+wVB6qRgs4EiTrNbRoZAHEFF5wRBf9gWn9HeoI66VtdMZvJRH+0/FDWB4/zwxS16n
+nADJaVPXh6JHJFYs9p0wZmvct3GNdWrOLRAG2yzbfFZS8fJcX1PYxXXo4By16yGW
+hQIDAQABo4IBYjCCAV4wEgYDVR0TAQH/BAgwBgEB/wIBADAdBgNVHQ4EFgQUO3DR
+U+l2JZ1gqMpmD8abrm9UFmowHwYDVR0jBBgwFoAUTiJUIBiV5uNu5g/6+rkS7QYX
+jzkwDgYDVR0PAQH/BAQDAgGGMB0GA1UdJQQWMBQGCCsGAQUFBwMBBggrBgEFBQcD
+AjB2BggrBgEFBQcBAQRqMGgwJAYIKwYBBQUHMAGGGGh0dHA6Ly9vY3NwLmRpZ2lj
+ZXJ0LmNvbTBABggrBgEFBQcwAoY0aHR0cDovL2NhY2VydHMuZGlnaWNlcnQuY29t
+L0RpZ2lDZXJ0R2xvYmFsUm9vdEcyLmNydDBCBgNVHR8EOzA5MDegNaAzhjFodHRw
+Oi8vY3JsMy5kaWdpY2VydC5jb20vRGlnaUNlcnRHbG9iYWxSb290RzIuY3JsMB0G
+A1UdIAQWMBQwCAYGZ4EMAQIBMAgGBmeBDAECAjANBgkqhkiG9w0BAQwFAAOCAQEA
+o9sJvBNLQSJ1e7VaG3cSZHBz6zjS70A1gVO1pqsmX34BWDPz1TAlOyJiLlA+eUF4
+B2OWHd3F//dJJ/3TaCFunjBhZudv3busl7flz42K/BG/eOdlg0kiUf07PCYY5/FK
+YTIch51j1moFlBqbglwkdNIVae2tOu0OdX2JiA+bprYcGxa7eayLetvPiA77ynTc
+UNMKOqYB41FZHOXe5IXDI5t2RsDM9dMEZv4+cOb9G9qXcgDar1AzPHEt/39335zC
+HofQ0QuItCDCDzahWZci9Nn9hb/SvAtPWHZLkLBG6I0iwGxvMwcTTc9Jnb4Flysr
+mQlwKsS2MphOoI23Qq3cSA==
+-----END CERTIFICATE-----
+)EOF";
+
 void setupTime() {
-  // Set timezone (e.g., UTC +1:00)
-  // Change according to your timezone
-  // For UTC -5:00, use -5 * 3600, and so on
-  configTime(1 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+ // Set timezone to Central European Time (CET) with daylight saving time (CEST)
+  // CET is UTC+1, CEST is UTC+2
+  // The timezone string format is: "TZ=std offset dst offset, start[/time], end[/time]"
+  // For CET/CEST: "CET-1CEST,M3.5.0/2,M10.5.0/3"
+  // This means:
+  // - Standard time is CET (UTC+1)
+  // - Daylight saving time is CEST (UTC+2)
+  // - DST starts on the last Sunday of March at 2:00 AM
+  // - DST ends on the last Sunday of October at 3:00 AM
+  configTzTime("CET-1CEST,M3.5.0/2,M10.5.0/3", "pool.ntp.org", "time.nist.gov");
 
   // Wait until time is set
   time_t now = time(nullptr);
@@ -48,8 +90,14 @@ void setupTime() {
 void updateFirmwareFromUrl(const String &firmwareUrl) {
     Serial.print("Downloading new firmware from: ");
     Serial.println(firmwareUrl);
+
+    digitalWrite(Red_LED_Pin, HIGH);
+    digitalWrite(Blue_LED_Pin, HIGH);
     
     t_httpUpdate_return ret = ESPhttpUpdate.update(firmwareUrl);
+    Serial.println("Firmware update process completed.");
+    Serial.print("HTTP Update result: ");
+    Serial.println(ret);
     
     switch(ret) {
         case HTTP_UPDATE_FAILED:
@@ -71,6 +119,9 @@ void updateFirmwareFromUrl(const String &firmwareUrl) {
             break;
     }
     Serial.println();
+
+    digitalWrite(Red_LED_Pin, LOW);
+    digitalWrite(Blue_LED_Pin, LOW);
 }
 
 void connectToMQTT() {
@@ -133,6 +184,7 @@ void setup() {
   setupTime();
   
   WiFiClientSecure client;
+  client.setCACert(ca_cert);
 
   if (!client.connect("iotstoragem1.blob.core.windows.net", 443)) {
     Serial.println("Connection failed!");
