f35af4ea52a13fc5ff2592b4c4ca028cf9c18713
Thomas Schissler <tschissler@agilemax.de>
2025-04-04 20:15:05 +0200

    Changed VWConenctor to carConnect

--- VWConnector/vw_mqtt.py
+++ VWConnector/vw_mqtt.py
@@ -2,11 +2,22 @@ from datetime import datetime, timedelta
 import os
 import asyncio
 import json
+from typing import Optional
+from carconnectivity import carconnectivity
+from carconnectivity.vehicle import ElectricVehicle
+from carconnectivity.commands import GenericCommand
+from carconnectivity.command_impl import ChargingStartStopCommand
 import paho.mqtt.client as mqtt
 from weconnect import weconnect
 from weconnect.domain import Domain
 from weconnect.elements.plug_status import PlugStatus
 from weconnect.elements.charging_status import ChargingStatus
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import List, Optional
+
+    from carconnectivity.garage import Garage
 
 #MQTT Broker settings
 MQTT_BROKER = 'smarthomepi2'
@@ -16,7 +27,35 @@ MQTT_TOPIC = 'data/charging/VW'
 def on_connect(client, userdata, flags, rc, properties):
     print(f"Connected with result code {rc}")
 
-async def fetch_vehicle_info():
+async def fetch_vehicle_info(carConnect: carconnectivity.CarConnectivity) -> dict:
+    carvin = os.getenv('VW_VIN')
+    garage: Optional[Garage] = carConnect.get_garage()
+    vehicle = garage.get_vehicle(carvin)
+
+    chargingEndTime = None
+    if vehicle.charging.state.value.value == ChargingStatus.ChargingState.CHARGING.value :
+        chargingEndTime = vehicle.charging.estimated_date_reached.value.isoformat()
+
+    return {
+        "nickname": vehicle.name.value,
+        "brand": "VW",
+        "name": vehicle.model.value,
+        "battery": vehicle.drives.drives["primary"].level.value,
+        "remainingRange": vehicle.drives.drives["primary"].range.value,
+        "mileage": vehicle.odometer.value,
+        "chargerConnected": vehicle.charging.connector.connection_state.value.value == "connected",
+        "chargingStatus": vehicle.charging.state.value.value,
+        "chargingTarget": vehicle.charging.settings.target_level.value,
+        "chargingEndTime": chargingEndTime,
+        "state": vehicle.state.value.value,
+        "position": {
+            "latitude": vehicle.position.latitude.value ,
+            "longitude": vehicle.position.longitude.value
+        },
+        "lastUpdate": vehicle.charging.state.last_updated_local.isoformat(),
+    }
+
+async def fetch_vehicle_info_weconnect():
     username = os.getenv('VW_USERNAME')
     password = os.getenv('VW_PASSWORD')
     carvin = os.getenv('VW_VIN')
@@ -54,13 +93,44 @@ async def fetch_vehicle_info():
 
 async def main():
 
+    print("Starting VW Connector")
+    print("##################################")
+    print()
+    print("Connecting to MQtT broker " + MQTT_BROKER + " on port " + str(MQTT_PORT) + " ...")
     client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
     client.on_connect = on_connect
     client.connect(MQTT_BROKER, MQTT_PORT, 60)
     client.loop_start()
+    print("successfully connected")
+
+    print("-----------------------------------")
+    print("Connecting to VW API ...")
+    username = os.getenv('VW_USERNAME')
+    password = os.getenv('VW_PASSWORD')
+    carvin = os.getenv('VW_VIN')
+
+    car_connectivity_config = {
+        "carConnectivity": {
+            "connectors": [
+                {
+                    "type": "volkswagen",
+                    "config": {
+                        "username": username,
+                        "password": password
+                    }
+                },
+            ]
+        }
+    }
+
+    carConnect = carconnectivity.CarConnectivity(config=car_connectivity_config, tokenstore_file="tokenstore.json")
+    carConnect.fetch_all()
+    garage: Optional[Garage] = carConnect.get_garage()
+    vehicle = garage.get_vehicle(carvin)
+    print("successfully connected for vehicle " + vehicle.name.value)
 
     while True:
-        result = await fetch_vehicle_info()
+        result = await fetch_vehicle_info(carConnect)
         payload = json.dumps(result)
         client.publish(MQTT_TOPIC, payload, qos=1, retain=True)
         print("Topic :" + MQTT_TOPIC + " | Message Sent: ", payload)
