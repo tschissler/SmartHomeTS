70c89b29f9409350a8f17f1fd96124b4919ab83c
Thomas Schissler <tschissler@agilemax.de>
2025-04-20 14:11:59 +0200

    Added missing files

---
+++ InfluxImporter/InfluxConnector/IInfluxConnector.cs
@@ -0,0 +1,11 @@
+﻿using InfluxDB.Client.Core.Flux.Domain;
+
+namespace InfluxConnector
+{
+    public interface IInfluxConnector
+    {
+        void EnsureBucketExists(string bucketName);
+        Task<List<FluxTable>> QueryDataAsync(string query);
+        void WritePointDataToInfluxDb(string bucketName, string measurementName, IDictionary<string, object> fields, IDictionary<string, string> tags);
+    }
+}
---
+++ InfluxImporter/InfluxConnector/PointDataExtensions.cs
@@ -0,0 +1,55 @@
+﻿using InfluxDB.Client.Writes;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfluxConnector
+{
+    public static class PointDataExtensions
+    {
+        public static PointData SetTags(this PointData point, IDictionary<string, string> tags)
+        {
+            foreach (var tag in tags)
+            {
+                point = point.Tag(tag.Key, tag.Value);
+            }
+            return point;
+        }
+
+        public static PointData SetFields(this PointData point, IDictionary<string, object> fields)
+        {
+            foreach (var field in fields)
+            {
+                if (field.Value is JValue jValue)
+                {
+                    switch (jValue.Type)
+                    {
+                        case JTokenType.Boolean:
+                            point = point.Field(field.Key, (bool)jValue.Value);
+                            break;
+                        case JTokenType.Integer:
+                            point = point.Field(field.Key, Convert.ToInt64(jValue.Value));
+                            break;
+                        case JTokenType.Float:
+                            point = point.Field(field.Key, Convert.ToDouble(jValue.Value));
+                            break;
+                        case JTokenType.String:
+                            point = point.Field(field.Key, jValue.Value?.ToString());
+                            break;
+                        case JTokenType.Date:
+                            point = point.Field(field.Key, ((DateTime)jValue.Value).ToString("o"));
+                            break;
+                    }
+                }
+                else
+                {
+                    point = point.Field(field.Key, field.Value);
+                }
+            }
+            return point;
+        }
+    }
+}
